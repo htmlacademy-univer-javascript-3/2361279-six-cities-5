ffeff1da0d85b3ab2b18d0b53ebde491cd615e48
Egor Usov <egotronn@gmail.com>
2024-10-03 15:22:26 +0500

    Исправил опечатку

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,5 +1,5 @@
 import {Main} from './main.tsx';
-import {PlaceCardProps} from './city_card.tsx';
+import {PlaceCardProps} from './place_card.tsx';
 
 export function App(props: { places_found: number; offers: PlaceCardProps[] }) {
   return <Main {...props}/>;
