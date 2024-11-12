54803ed509d154f5bd232745c9628d6d030a3f09
Egor Usov <egotronn@gmail.com>
2024-11-12 23:19:13 +0500

    Исправил замечения Typescript

--- src/shared/mocks/favorite.ts
+++ src/shared/mocks/favorite.ts
@@ -5,18 +5,21 @@ export const mockFavoritePlaces: Place[] = [
     id: 0,
     rating: 5, cardType: PlaceType.Apartment,
     name: 'nice, cozy, warm big bed apartment', price: 180,
-    imageName: 'apartment-03.jpg'
+    imageName: 'apartment-03.jpg',
+    coordinates: {latitude: 52.3909553943508, longitude: 4.85309666406198}
   },
   {
     id: 1,
     rating: 4, cardType: PlaceType.Room,
     name: 'wood and stone place', price: 80,
-    imageName: 'room.jpg'
+    imageName: 'room.jpg',
+    coordinates: {latitude: 52.3609553943508, longitude: 4.85309666406198}
   },
   {
     id: 2,
     rating: 5, cardType: PlaceType.Apartment,
     name: 'white castle', price: 180,
-    imageName: 'apartment-small-04.jpg'
+    imageName: 'apartment-small-04.jpg',
+    coordinates: {latitude: 52.3909553943508, longitude: 4.929309666406198}
   },
 ];
