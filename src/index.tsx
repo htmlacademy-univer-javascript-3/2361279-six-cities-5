6d33a92f0a614fe3518561e90c3e62bf367dd3be
Egor Usov <egotronn@gmail.com>
2024-11-20 21:13:00 +0500

    Исправил ошибки tsc

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {App} from './components/app.tsx';
-import {mockOfferPlaces} from './shared/mocks/offer.ts';
 import {mockFavoritePlaces} from './shared/mocks/favorite.ts';
 import {Provider} from 'react-redux';
 import {store} from './store/index.ts';
@@ -13,7 +12,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App mainPagePlaces={mockOfferPlaces} favoritePlaces={mockFavoritePlaces}/>
+      <App favoritePlaces={mockFavoritePlaces}/>
     </Provider>
   </React.StrictMode>
 );
